05fe38fd2d29c023b0f820228aa4271e6b3d24e2
HomeFarm User <user@homefarm.local>
2025-12-16 02:52:21 +0100

    fix(chore): update vite config to shim process.env for CRA compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env vars regardless of prefix, 
+  // or 'REACT_APP_' to specificially load those. We'll use 'REACT_APP_' to match CRA.
+  const env = loadEnv(mode, process.cwd(), 'REACT_APP_');
+
+  return {
+    plugins: [react()],
+    base: './',
+    define: {
+      // Expose env vars to the client
+      'process.env': env
+    }
+  }
 })
